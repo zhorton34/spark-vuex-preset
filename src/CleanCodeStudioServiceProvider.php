e5c2c2219e278bcae2f7acf578b7a69da443ac67
Zak <zhorton999@gmail.com>
2019-01-16 03:36:46 -0600

    #1 - added console command for spark vuex preset

--- src/CleanCodeStudioServiceProvider.php
+++ src/CleanCodeStudioServiceProvider.php
@@ -4,11 +4,18 @@ namespace CleanCodeStudio\SparkVuexPreset;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\Console\PresetCommand;
-
+use Commands\ScaffoldVuexForLaravelSpark;
 class CleanCodeStudioServiceProvider extends ServiceProvider
 {
 	 public function boot()
 	 {
+
+	 	    if ($this->app->runningInConsole()) {
+	        $this->commands([
+  	          ScaffoldVuexForLaravelSpark::class,
+    	    ]);
+    		}
+		 		
 		 		PresetCommand::macro('clean:spark-vuex', function ($command) {
 			
 						SparkVuexPreset::install();
